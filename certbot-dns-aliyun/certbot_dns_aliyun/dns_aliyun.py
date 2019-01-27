0c0964b7234b3c3d55598bc9425bf2f6215b3923
kaiserli <kaiserli@gmail.com>
2019-01-28 01:22:04 +0800

    add dns-aliyun

---
+++ certbot-dns-aliyun/certbot_dns_aliyun/dns_aliyun.py
@@ -0,0 +1,181 @@
+"""DNS Authenticator for Aliyun."""
+import logging
+
+
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.request import CommonRequest
+import zope.interface
+
+
+from certbot import errors
+from certbot import interfaces
+from certbot.plugins import dns_common
+
+logger = logging.getLogger(__name__)
+
+ACCOUNT_URL = 'https://usercenter.console.aliyun.com/#/manage/ak'
+
+
+@zope.interface.implementer(interfaces.IAuthenticator)
+@zope.interface.provider(interfaces.IPluginFactory)
+class Authenticator(dns_common.DNSAuthenticator):
+    """DNS Authenticator for Aliyun
+
+    This Authenticator uses the Aliyun API to fulfill a dns-01 challenge.
+    """
+
+    description = ('Obtain certificates using a DNS TXT record (if you are using Aliyun for '
+                   'DNS).')
+    ttl = 120
+
+    def __init__(self, *args, **kwargs):
+        super(Authenticator, self).__init__(*args, **kwargs)
+        self.credentials = None
+
+    @classmethod
+    def add_parser_arguments(cls, add):  # pylint: disable=arguments-differ
+        super(Authenticator, cls).add_parser_arguments(add)
+        add('credentials', help='Aliyun credentials INI file.')
+
+    def more_info(self):  # pylint: disable=missing-docstring,no-self-use
+        return 'This plugin configures a DNS TXT record to respond to a dns-01 challenge using ' + \
+               'the Aliyun API.'
+
+    def _setup_credentials(self):
+        self.credentials = self._configure_credentials(
+            'credentials',
+            'Aliyun credentials INI file',
+            {
+                'access-key-id': 'AccessKeyId obtained from {0}'.format(ACCOUNT_URL),
+                'access-key-secret': 'AccessKeySecret of the AccessKeyId',
+            }
+        )
+
+    def _perform(self, domain, validation_name, validation):
+        self._get_aliyun_client().add_txt_record(domain, validation_name, validation, self.ttl)
+
+    def _cleanup(self, domain, validation_name, validation):
+        self._get_aliyun_client().del_txt_record(domain, validation_name, validation)
+
+    def _get_aliyun_client(self):
+        return _AliyunClient(self.credentials.conf('access-key-id'), self.credentials.conf('access-key-secret'))
+
+
+class _AliyunClient(object):
+    """
+    Encapsulates all communication with the Aliyun API.
+    """
+
+    def __init__(self, accessKeyId, accessSecret):        
+        self.client = AcsClient(accessKeyId, accessSecret, 'default')
+
+    def _common_request(self, apiName):
+        request = CommonRequest()
+        request.set_accept_format('json')
+        request.set_domain('alidns.aliyuncs.com')
+        request.set_method('POST')
+        request.set_version('2015-01-09')
+        request.set_action_name(apiName) 
+        return request
+
+    def add_txt_record(self, domain, record_name, record_content, record_ttl):
+        """
+        Add a TXT record using the supplied information.
+
+        :param str domain: The domain to use to look up the Aliyun zone.
+        :param str record_name: The record name (typically beginning with '_acme-challenge.').
+        :param str record_content: The record content (typically the challenge validation).
+        :param int record_ttl: The record TTL (number of seconds that the record may be cached).
+        :raises certbot.errors.PluginError: if an error occurs communicating with the Aliyun API
+        """
+
+        try:            
+            request = self._common_request("AddDomainRecord")
+
+            request.add_query_param('DomainName', domain)
+            request.add_query_param('RR', record_name)
+            request.add_query_param('Type', 'TXT')
+            request.add_query_param('Value', record_content)
+            request.add_query_param('TTL', record_ttl)
+
+            response = self.client.do_action(request)
+        except Exception as e:
+            logger.error('Encountered Aliyun APII Error adding TXT record: %s', e)
+            raise errors.PluginError('Error communicating with the Aliyun API: {0}'.format(e))
+
+        rsp = json.loads(response, encoding='utf-8')
+
+        if "Code" in rsp:
+            err = 'Encountered Aliyun adding TXT error: {0}'.format(rsp["Message"])
+            logger.debug(err)
+            raise errors.PluginError(err)
+
+        logger.debug('Successfully added TXT record with record_id: %s', rsp["RecordId"])
+
+    def del_txt_record(self, domain, record_name, record_content):
+        """
+        Delete a TXT record using the supplied information.
+
+        Note that both the record's name and content are used to ensure that similar records
+        created concurrently (e.g., due to concurrent invocations of this plugin) are not deleted.
+
+        Failures are logged, but not raised.
+
+        :param str domain: The domain to use to look up the Aliyun zone.
+        :param str record_name: The record name (typically beginning with '_acme-challenge.').
+        :param str record_content: The record content (typically the challenge validation).
+        """
+
+        record_id = self._find_txt_record_id(domain, record_name, record_content)
+        if record_id:
+            try:
+                request = self._common_request("DeleteDomainRecord")
+                request.add_query_param('RecordId', record_id)                
+                
+                response = client.do_action(request)
+
+                rsp = json.loads(response, encoding='utf-8')
+
+                if "Code" in rsp:
+                    raise rsp["Message"]
+
+                logger.debug('Successfully deleted TXT record.')
+            except Exception as e:
+                logger.warning('Encountered aliyun deleting TXT record: %s', e)
+        else:
+            logger.debug('TXT record not found; no cleanup needed.')
+
+    def _find_txt_record_id(self, domain, record_name, record_content):
+        """
+        Find the record_id for a TXT record with the given name and content.
+
+        :param str domain: The domain which contains the record.
+        :param str record_name: The record name (typically beginning with '_acme-challenge.').
+        :param str record_content: The record content (typically the challenge validation).
+        :returns: The record_id, if found.
+        :rtype: str
+        """
+
+        try:
+            request = self._common_request('DescribeSubDomainRecords')
+
+            request.add_query_param('SubDomain', record_name + domain)
+            request.add_query_param('Type', 'TXT')
+            response = client.do_action(request)
+        except Exception as e:
+            logger.debug('Encountered Aliyun getting TXT error: %s', e)
+            return
+
+        rsp = json.loads(response, encoding='utf-8')
+
+        if "Code" in rsp:
+            logger.debug('Encountered Aliyun getting TXT error: %s', rsp["Message"])
+            return
+        
+        if len(rsp["DomainRecords"]["Record"]) > 0:
+            # Cleanup is returning the system to the state we found it. If, for some reason,
+            # there are multiple matching records, we only delete one because we only added one.
+
+            return rsp["DomainRecords"]["Record"][0]["RecordId"]
+        else:
+            logger.debug('Unable to find TXT record.')
---
+++ certbot-dns-aliyun/certbot_dns_aliyun/dns_aliyun_test.py
@@ -0,0 +1,175 @@
+"""Tests for certbot_dns_aliyun.dns_aliyun."""
+
+import os
+import unittest
+
+
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.request import CommonRequest
+import mock
+
+from certbot import errors
+from certbot.plugins import dns_test_common
+from certbot.plugins.dns_test_common import DOMAIN
+from certbot.tests import util as test_util
+
+ACCESS_KEY_ID = 'my-access-key'
+ACCESS_KEY_SECRET = 'my-access-secret'
+
+
+class AuthenticatorTest(test_util.TempDirTestCase, dns_test_common.BaseAuthenticatorTest):
+
+    def setUp(self):
+        from certbot_dns_aliyun.dns_aliyun import Authenticator
+
+        super(AuthenticatorTest, self).setUp()
+
+        path = os.path.join(self.tempdir, 'file.ini')
+        dns_test_common.write({"access-key-id": ACCESS_KEY_ID, "access-key-secret": ACCESS_KEY_SECRET}, path)
+
+        self.config = mock.MagicMock(aliyun_credentials=path,
+                                     aliyun_propagation_seconds=0)  # don't wait during tests
+
+        self.auth = Authenticator(self.config, "aliyun")
+
+        self.mock_client = mock.MagicMock()
+        # _get_aliyun_client | pylint: disable=protected-access
+        self.auth._get_aliyun_client = mock.MagicMock(return_value=self.mock_client)
+
+    def test_perform(self):
+        self.auth.perform([self.achall])
+
+        expected = [mock.call.add_txt_record(DOMAIN, '_acme-challenge.'+DOMAIN, mock.ANY, mock.ANY)]
+        self.assertEqual(expected, self.mock_client.mock_calls)
+
+    def test_cleanup(self):
+        # _attempt_cleanup | pylint: disable=protected-access
+        self.auth._attempt_cleanup = True
+        self.auth.cleanup([self.achall])
+
+        expected = [mock.call.del_txt_record(DOMAIN, '_acme-challenge.'+DOMAIN, mock.ANY)]
+        self.assertEqual(expected, self.mock_client.mock_calls)
+
+
+class AliyunClientTest(unittest.TestCase):
+    record_name = "foo"
+    record_content = "bar"
+    record_ttl = 42
+    zone_id = 1
+    record_id = 2
+
+    def setUp(self):
+        from certbot_dns_aliyun.dns_aliyun import _AliyunClient
+
+        self.aliyun_client = _AliyunClient(EMAIL, API_KEY)
+
+        self.cf = mock.MagicMock()
+        self.aliyun_client.cf = self.cf
+
+    def test_add_txt_record(self):
+        self.cf.zones.get.return_value = [{'id': self.zone_id}]
+
+        self.aliyun_client.add_txt_record(DOMAIN, self.record_name, self.record_content,
+                                              self.record_ttl)
+
+        self.cf.zones.dns_records.post.assert_called_with(self.zone_id, data=mock.ANY)
+
+        post_data = self.cf.zones.dns_records.post.call_args[1]['data']
+
+        self.assertEqual('TXT', post_data['type'])
+        self.assertEqual(self.record_name, post_data['name'])
+        self.assertEqual(self.record_content, post_data['content'])
+        self.assertEqual(self.record_ttl, post_data['ttl'])
+
+    def test_add_txt_record_error(self):
+        self.cf.zones.get.return_value = [{'id': self.zone_id}]
+
+        self.cf.zones.dns_records.post.side_effect = API_ERROR
+
+        self.assertRaises(
+            errors.PluginError,
+            self.aliyun_client.add_txt_record,
+            DOMAIN, self.record_name, self.record_content, self.record_ttl)
+
+    def test_add_txt_record_error_during_zone_lookup(self):
+        self.cf.zones.get.side_effect = API_ERROR
+
+        self.assertRaises(
+            errors.PluginError,
+            self.aliyun_client.add_txt_record,
+            DOMAIN, self.record_name, self.record_content, self.record_ttl)
+
+    def test_add_txt_record_zone_not_found(self):
+        self.cf.zones.get.return_value = []
+
+        self.assertRaises(
+            errors.PluginError,
+            self.aliyun_client.add_txt_record,
+            DOMAIN, self.record_name, self.record_content, self.record_ttl)
+
+    def test_del_txt_record(self):
+        self.cf.zones.get.return_value = [{'id': self.zone_id}]
+        self.cf.zones.dns_records.get.return_value = [{'id': self.record_id}]
+
+        self.aliyun_client.del_txt_record(DOMAIN, self.record_name, self.record_content)
+
+        expected = [mock.call.zones.get(params=mock.ANY),
+                    mock.call.zones.dns_records.get(self.zone_id, params=mock.ANY),
+                    mock.call.zones.dns_records.delete(self.zone_id, self.record_id)]
+
+        self.assertEqual(expected, self.cf.mock_calls)
+
+        get_data = self.cf.zones.dns_records.get.call_args[1]['params']
+
+        self.assertEqual('TXT', get_data['type'])
+        self.assertEqual(self.record_name, get_data['name'])
+        self.assertEqual(self.record_content, get_data['content'])
+
+    def test_del_txt_record_error_during_zone_lookup(self):
+        self.cf.zones.get.side_effect = API_ERROR
+
+        self.aliyun_client.del_txt_record(DOMAIN, self.record_name, self.record_content)
+
+    def test_del_txt_record_error_during_delete(self):
+        self.cf.zones.get.return_value = [{'id': self.zone_id}]
+        self.cf.zones.dns_records.get.return_value = [{'id': self.record_id}]
+        self.cf.zones.dns_records.delete.side_effect = API_ERROR
+
+        self.aliyun_client.del_txt_record(DOMAIN, self.record_name, self.record_content)
+        expected = [mock.call.zones.get(params=mock.ANY),
+                    mock.call.zones.dns_records.get(self.zone_id, params=mock.ANY),
+                    mock.call.zones.dns_records.delete(self.zone_id, self.record_id)]
+
+        self.assertEqual(expected, self.cf.mock_calls)
+
+    def test_del_txt_record_error_during_get(self):
+        self.cf.zones.get.return_value = [{'id': self.zone_id}]
+        self.cf.zones.dns_records.get.side_effect = API_ERROR
+
+        self.aliyun_client.del_txt_record(DOMAIN, self.record_name, self.record_content)
+        expected = [mock.call.zones.get(params=mock.ANY),
+                    mock.call.zones.dns_records.get(self.zone_id, params=mock.ANY)]
+
+        self.assertEqual(expected, self.cf.mock_calls)
+
+    def test_del_txt_record_no_record(self):
+        self.cf.zones.get.return_value = [{'id': self.zone_id}]
+        self.cf.zones.dns_records.get.return_value = []
+
+        self.aliyun_client.del_txt_record(DOMAIN, self.record_name, self.record_content)
+        expected = [mock.call.zones.get(params=mock.ANY),
+                    mock.call.zones.dns_records.get(self.zone_id, params=mock.ANY)]
+
+        self.assertEqual(expected, self.cf.mock_calls)
+
+    def test_del_txt_record_no_zone(self):
+        self.cf.zones.get.return_value = [{'id': None}]
+
+        self.aliyun_client.del_txt_record(DOMAIN, self.record_name, self.record_content)
+        expected = [mock.call.zones.get(params=mock.ANY)]
+
+        self.assertEqual(expected, self.cf.mock_calls)
+
+
+if __name__ == "__main__":
+    unittest.main()  # pragma: no cover
